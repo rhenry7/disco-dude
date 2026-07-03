9177cde8d01221207788fef6eab5a667303852fb
Ramone Henry <ramoneh94@gmail.com>
2026-07-02 20:51:06 -0400

    delete unused image nodes; adjust window size; adjust ground floor image

--- player.gd
+++ player.gd
@@ -2,13 +2,23 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -600.0
+const SPRING_VELOCITY = -700.0
 
 func _ready() -> void:
 	$Sprite.play("idle")
 
 
+func _respawn() -> void:
+	position = Vector2(55, 0)
+	velocity = Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
+	var screen_bottom := get_viewport_rect().size.y - get_viewport_transform().get_origin().y
+	if position.y > screen_bottom:
+		_respawn()
+		return
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -34,3 +44,16 @@ func _physics_process(delta: float) -> void:
 			$Sprite.play("idle")
 
 	move_and_slide()
+
+	for i in get_slide_collision_count():
+		var col := get_slide_collision(i)
+		if col == null:
+			continue
+		var collider := col.get_collider()
+		if collider == null:
+			continue
+		if collider.is_in_group("enemy") and col.get_normal().y < -0.5:
+			velocity.y = SPRING_VELOCITY
+			$Sprite.play("jump")
+			await $Sprite.animation_finished
+			$Sprite.play("idle")
